d1a8876c003684db9fda1a328131a5983318ee57
Ethan Zeigler <ethanzeigler@gmail.com>
2024-05-05 15:34:17 -0400

    Deployable MVP

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { analyzer } from "vite-bundle-analyzer";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    //VueDevTools(),
+    VueDevTools(),
+    ViteImageOptimizer(),
+    //analyzer(),
   ],
   resolve: {
     alias: {
