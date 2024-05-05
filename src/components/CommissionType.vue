0e2129b55118dc1be537c1541c40b7d0bb0623bb
Ethan Zeigler <ethanzeigler@gmail.com>
2024-05-05 14:42:21 -0400

    Commission behavior complete

--- src/components/CommissionType.vue
+++ src/components/CommissionType.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="type">
         <h2>{{ props.name }}</h2>
-        <div class="example", :class="{ large: props.examples.size == 'large' }">
+        <div class="example" :class="{ large: props.examples.size == 'large' }">
             <img
                 v-for="example in props.examples.files"
-                :src="example.file",
-                :class="{ 'desktop-only': example.desktopOnly }">
+                :src="example.file"
+                :class="{ 'desktop-only': example.desktopOnly }"
                 alt=""
             >
         </div>
@@ -14,6 +14,7 @@
             <div
                 v-for="product in props.products"
                 class="product-card"
+                :class="{ 'wide': (product.size == 'wide') }"
             >
                 <h3>{{ product.title }}</h3>
                 <ul>
@@ -21,142 +22,48 @@
                 </ul>
                 <div class="grow-spacer"></div>
                 <div class="prices">
-                    <ol v-for="">
-                        <li>Sketch</li>
-                        <li>$50 USD</li>
-                    </ol>
-                    <ol>
-                        <li>Clean</li>
-                        <li>$75 USD</li>
-                    </ol>
-                </div>
-            </div>
-            <div class="product-card">
-                <h3>Standard Commission</h3>
-                <ul>
-                    <li>Client gives references/inspirations</li>
-                    <li>WIPs for each progress stage</li>
-                    <li>3-4 edits allowed</li>
-                    <li>Additional edits with a fee</li>
-                </ul>
-                <div class="grow-spacer"></div>
-                <div class="prices">
-                    <ol>
-                        <li>Sketch</li>
-                        <li>$70 USD</li>
-                    </ol>
-                    <ol>
-                        <li>Clean</li>
-                        <li>$95 USD</li>
-                    </ol>
-                </div>
-            </div>
-            <div class="product-card">
-                <h3>Full Input</h3>
-                <ul>
-                    <li>Client gives references/inspirations</li>
-                    <li>WIPs for each progress stage</li>
-                    <li>Unlimited input and edits (within reason)</li>
-                </ul>
-                <div class="prices">
-                    <ol>
-                        <li>Sketch</li>
-                        <li>$100 USD</li>
-                    </ol>
-                    <ol>
-                        <li>Clean</li>
-                        <li>$150 USD</li>
+                    <ol v-for="priceName in Object.keys(product.prices)">
+                        <li>{{ priceName }}</li>
+                        <li>{{ product.prices[priceName] }}</li>
                     </ol>
                 </div>
             </div>
         </div>
 
-        <details>
-            <summary>Add-ons &#11206;</summary>
+        <details
+            v-for="[name, products] of Object.entries(props.collapsedProducts)">
+            <summary>{{ name }}</summary>
             <div class="products">
-                <div class="product-card">
-                    <h3>Complexity Fee</h3>
+                <div
+                    v-for="product in products"
+                    class="product-card"
+                    :class="{ 'wide': (product.size == 'wide') }"
+                >
+                    <h3>{{ product.title }}</h3>
                     <ul>
-                        <li>For very complex designs, an extra fee is added</li>
-                        <li>Does not apply to Artist's Choice</li>
-                    </ul>
-                    <div class="prices">
-                        <ol>
-                            <li>Sketch</li>
-                            <li>20% of total price</li>
-                        </ol>
-                        <ol>
-                            <li>Clean</li>
-                            <li>40% of total price</li>
-                        </ol>
-                    </div>
-                </div>
-                <div class="product-card">
-                    <h3>Clothing Design</h3>
-                    <ul>
-                        <li>Add clothing to your character, which will be drawn on a copied fullbody by default
-                        </li>
-                        <li>For a new pose, you must ask for an extra fullbody on top</li>
-                    </ul>
-                    <div class="prices">
-                        <ol>
-                            <li>Sketch</li>
-                            <li>+$30 USD</li>
-                        </ol>
-                        <ol>
-                            <li>Clean</li>
-                            <li>+$45 usd</li>
-                        </ol>
-                    </div>
-                </div>
-                <div class="product-card">
-                    <h3>Halfbody</h3>
-                    <ul>
-                        <li>Additional halfbody - drawing from the shouders and up - accompanying your design.
-                        </li>
+                        <li v-for="feature in product.features">{{ feature }}</li>
                     </ul>
                     <div class="grow-spacer"></div>
                     <div class="prices">
-                        <ol>
-                            <li>Sketch</li>
-                            <li>+$25 USD</li>
-                        </ol>
-                        <ol>
-                            <li>Clean</li>
-                            <li>+$40 usd</li>
+                        <ol v-for="priceName in Object.keys(product.prices)">
+                            <li>{{ priceName }}</li>
+                            <li>{{ product.prices[priceName] }}</li>
                         </ol>
                     </div>
                 </div>
-                <div class="product-card">
-                    <h3>Fullbody</h3>
-                    <ul>
-                        <li>Additional fullbody accompanying your design.</li>
-                    </ul>
-                    <div class="prices">
-                        <ol>
-                            <li>Sketch</li>
-                            <li>+$50 USD</li>
-                        </ol>
-                        <ol>
-                            <li>Clean</li>
-                            <li>+$75 usd</li>
-                        </ol>
-                    </div>
-                </div>
-
             </div>
         </details>
-
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 const props = defineProps({
     name: String,
     description: String,
     examples: Object,
     products: Array,
+    collapsedProducts: Object,
 })
 
 
--- src/views/CommissionView.vue
+++ src/views/CommissionView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import TabBar from "@/components/TabBar.vue"
+import CommissionType from "@/components/CommissionType.vue"
 
 const selectedTab = ref("Commission Info")
 const tabs = ["Commission Info", "Terms of Service", "Order"]
@@ -13,16 +14,16 @@ const comTypes = [
             size: "large",
             files: [
                 {
-                    file: "@/assets/images/poufdesign2.png",
+                    file: "images/poufdesign2.png",
                     desktopOnly: false,
                 },
                 {
-                    file: "@/assets/images/poufdesign3.png",
+                    file: "images/poufdesign3.png",
                     desktopOnly: true,
                 },
                 {
-                    file: '@/assets/images/luminae.png',
-                    desktopOnly: true
+                    file: 'images/luminae.png',
+                    desktopOnly: true,
                 },
             ]
         },
@@ -36,9 +37,8 @@ const comTypes = [
                 ],
                 prices: {
                     "Sketch": "$50 USD",
-                    "Clean": "$75 USD"
+                    "Clean": "$75 USD",
                 },
-                collapsed: false
             },
             {
                 title: "Standard Commission",
@@ -50,9 +50,8 @@ const comTypes = [
                 ],
                 prices: {
                     "Sketch": "$70 USD",
-                    "Clean": "$95 USD"
+                    "Clean": "$95 USD",
                 },
-                collapsed: false,
             },
             {
                 title: "Full Input",
@@ -63,24 +62,102 @@ const comTypes = [
                 ],
                 prices: {
                     "Sketch": "$100 USD",
-                    "Clean": "150 USD"
+                    "Clean": "$150 USD",
                 },
-                collapsed: false,
             },
+        ],
+        collapsedProducts: {
+            "Add-ons": [
+                {
+                    title: "Complexity Fee",
+                    features: [
+                        "For very complex designs, an extra fee is added",
+                        "Does not apply to Artist's Choice",
+                    ],
+                    prices: {
+                        "Sketch": "20% of total price",
+                        "Clean": "40% of total price",
+                    },
+                },
+                {
+                    title: "Clothing Design",
+                    features: [
+                        "Add clothing to your character, which will be drawn on a copied fullbody by default",
+                        "For a new pose, you must ask for an extra fullbody on top",
+                    ],
+                    prices: {
+                        "Sketch": "+$30 USD",
+                        "Clean": "+$45 USD",
+                    },
+                },
+                {
+                    title: "Halfbody",
+                    features: [
+                        "Additional halfbody - drawing from the shouders and up - accompanying your design.",
+                    ],
+                    prices: {
+                        "Sketch": "+$25 USD",
+                        "Clean": "+$40 USD",
+                    },
+                },
+                {
+                    title: "Fullbody",
+                    features: [
+                        "Additional fullbody accompanying your design.",
+                    ],
+                    prices: {
+                        "Sketch": "+$50 USD",
+                        "Clean": "+$75 USD",
+                    },
+                },
+            ],
+        }
+    }, // end Custom Designs
+    {
+        name: "Minis",
+        description: "A mini drawing of your character. Perfect for icons, pagedolls, decorations, etc. Design will be simplified to fit the style.",
+        examples: {
+            files: [
+                {
+                    file: "images/clara.png",
+                    desktopOnly: false,
+                },
+                {
+                    file: "images/wisp.png",
+                    desktopOnly: true,
+                },
+                {
+                    file: "images/sadbloom.png",
+                    desktopOnly: true,
+                },
+                {
+                    file: 'images/will.png',
+                    desktopOnly: true,
+                },
+                {
+                    file: "images/minime.png",
+                    desktopOnly: true,
+                }
+            ]
+        },
+        products: [
             {
-                title: "Complexity Fee",
+                title: "Standard",
+                size: "wide",
                 features: [
-                    "For very complex designs, an extra fee is added",
-                    "Does not apply to Artist's Choice",
+                    "1000x1000 drawing of your character. Can be made smaller/nonsquare.",
+                    "One WIP sketch",
+                    "Preferred pose/expression allowed with size considerations",
+                    "Small edits allowed but new poses have a fee",
                 ],
                 prices: {
-                    "Sketch": "20% of total price",
-                    "Clean": "40% of total price",
+                    "Clean": "$50 USD",
+                    "Pose Change": "+$10 USD"
                 },
-                collapsed: "Add-ons"
-            }
-        ]
-    }
+            },
+        ],
+        collapsedProducts: [],
+    },
 ]
 
 </script>
@@ -89,217 +166,18 @@ const comTypes = [
     <div class="content commission-content">
         <TabBar v-model="selectedTab" :tabs="tabs"></TabBar>
 
-        <div id="commission-info" class="gallery">
-            <div class="type">
-                <h2>Custom Design</h2>
-                <div class="example large">
-                    <a href="images/poufdesign2.png" data-lightbox="gallery"><img src="@/assets/images/poufdesign2.png"
-                            alt=""></a>
-                    <a class="desktop-only" href="images/poufdesign3.png" data-lightbox="gallery"><img
-                            src="@/assets/images/poufdesign3.png" alt=""></a>
-                    <a class="desktop-only" href="images/luminae.png" data-lightbox="gallery">
-                        <img src="@/assets/images/luminae.png" alt="">
-                    </a>
-                </div>
-                <p>
-                    A custom character, designed under your requests and preferences.
-                    Base customs come with one fullbody drawing.
-                </p>
-
-                <div class="products">
-                    <div class="product-card">
-                        <h3>Artist's Choice</h3>
-                        <ul>
-                            <li>Client gives references/inspirations</li>
-                            <li>No WIPs</li>
-                            <li>No edits</li>
-                        </ul>
-                        <div class="grow-spacer"></div>
-                        <div class="prices">
-                            <ol>
-                                <li>Sketch</li>
-                                <li>$50 USD</li>
-                            </ol>
-                            <ol>
-                                <li>Clean</li>
-                                <li>$75 USD</li>
-                            </ol>
-                        </div>
-                    </div>
-                    <div class="product-card">
-                        <h3>Standard Commission</h3>
-                        <ul>
-                            <li>Client gives references/inspirations</li>
-                            <li>WIPs for each progress stage</li>
-                            <li>3-4 edits allowed</li>
-                            <li>Additional edits with a fee</li>
-                        </ul>
-                        <div class="grow-spacer"></div>
-                        <div class="prices">
-                            <ol>
-                                <li>Sketch</li>
-                                <li>$70 USD</li>
-                            </ol>
-                            <ol>
-                                <li>Clean</li>
-                                <li>$95 USD</li>
-                            </ol>
-                        </div>
-                    </div>
-                    <div class="product-card">
-                        <h3>Full Input</h3>
-                        <ul>
-                            <li>Client gives references/inspirations</li>
-                            <li>WIPs for each progress stage</li>
-                            <li>Unlimited input and edits (within reason)</li>
-                        </ul>
-                        <div class="prices">
-                            <ol>
-                                <li>Sketch</li>
-                                <li>$100 USD</li>
-                            </ol>
-                            <ol>
-                                <li>Clean</li>
-                                <li>$150 USD</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div>
-
-                <details>
-                    <summary>Add-ons &#11206;</summary>
-                    <div class="products">
-                        <div class="product-card">
-                            <h3>Complexity Fee</h3>
-                            <ul>
-                                <li>For very complex designs, an extra fee is added</li>
-                                <li>Does not apply to Artist's Choice</li>
-                            </ul>
-                            <div class="prices">
-                                <ol>
-                                    <li>Sketch</li>
-                                    <li>20% of total price</li>
-                                </ol>
-                                <ol>
-                                    <li>Clean</li>
-                                    <li>40% of total price</li>
-                                </ol>
-                            </div>
-                        </div>
-                        <div class="product-card">
-                            <h3>Clothing Design</h3>
-                            <ul>
-                                <li>Add clothing to your character, which will be drawn on a copied fullbody by default
-                                </li>
-                                <li>For a new pose, you must ask for an extra fullbody on top</li>
-                            </ul>
-                            <div class="prices">
-                                <ol>
-                                    <li>Sketch</li>
-                                    <li>+$30 USD</li>
-                                </ol>
-                                <ol>
-                                    <li>Clean</li>
-                                    <li>+$45 usd</li>
-                                </ol>
-                            </div>
-                        </div>
-                        <div class="product-card">
-                            <h3>Halfbody</h3>
-                            <ul>
-                                <li>Additional halfbody - drawing from the shouders and up - accompanying your design.
-                                </li>
-                            </ul>
-                            <div class="grow-spacer"></div>
-                            <div class="prices">
-                                <ol>
-                                    <li>Sketch</li>
-                                    <li>+$25 USD</li>
-                                </ol>
-                                <ol>
-                                    <li>Clean</li>
-                                    <li>+$40 usd</li>
-                                </ol>
-                            </div>
-                        </div>
-                        <div class="product-card">
-                            <h3>Fullbody</h3>
-                            <ul>
-                                <li>Additional fullbody accompanying your design.</li>
-                            </ul>
-                            <div class="prices">
-                                <ol>
-                                    <li>Sketch</li>
-                                    <li>+$50 USD</li>
-                                </ol>
-                                <ol>
-                                    <li>Clean</li>
-                                    <li>+$75 usd</li>
-                                </ol>
-                            </div>
-                        </div>
-
-                    </div>
-                </details>
-
-            </div>
-            <!-- SCENES -->
-            <div class="type">
-                <h2>Minis</h2>
-                <div class="example">
-                    <a href="images/clara.png" data-lightbox="gallery"><img src="@/assets/images/clara.png" alt=""></a>
-                    <a class="desktop-only" href="images/wisp.png" data-lightbox="gallery"><img
-                            src="@/assets/images/wisp.png" alt=""></a>
-                    <a class="desktop-only" href="images/sadbloom.png" data-lightbox="gallery"><img
-                            src="@/assets/images/sadbloom.png" alt=""></a>
-                    <a class="desktop-only" href="images/will.png" data-lightbox="gallery"><img
-                            src="@/assets/images/will.png" alt=""></a>
-                    <a class="desktop-only" href="images/minime.png" data-lightbox="gallery"><img
-                            src="@/assets/images/minime.png" alt=""></a>
-                </div>
-                <p>
-                    A mini drawing of your character.
-                </p>
-                <div class="products twosquare">
-                    <div class="product-card">
-                        <h3>Details</h3>
-                        <ul>
-                            <li>Fit to square proportions. Non-square minis available upon request</li>
-                            <li>Perfect for icons, pagedolls, decorations, etc.</li>
-                            <li>Default size is 1000x1000. Smaller sizes given upon request</li>
-                            <li>Design will be simplified to fit the style</li>
-                        </ul>
-                        <div class="grow-spacer"></div>
-                        <div class="prices">
-                            <ol>
-                                <li>Price</li>
-                                <li>$50 USD</li>
-                            </ol>
-                        </div>
-                    </div>
-                    <div class="product-card">
-                        <h3>Edit Fee</h3>
-                        <ul>
-                            <li>Minis have one WIP stage, being the sketch</li>
-                            <li>Client can give prefered pose and expression, but size limitations may override this
-                                request</li>
-                            <li>Edits to the pose can be asked, but new poses come with the fee</li>
-                        </ul>
-                        <div class="grow-spacer"></div>
-                        <div class="prices">
-                            <ol>
-                                <li>Price</li>
-                                <li>$10 USD</li>
-                            </ol>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+        <div v-if="selectedTab == 'Commission Info'" id="commission-info" class="gallery">
+           <CommissionType
+                v-for="comType in comTypes"
+                :name="comType.name"
+                :description="comType.description"
+                :examples="comType.examples"
+                :products="comType.products"
+                :collapsedProducts="comType.collapsedProducts"
+            />
         </div>
 
-
-        <div id="terms-of-service" class="gallery terms-of-service-container" style="display:none">
+        <div v-if="selectedTab == 'Terms of Service'" id="terms-of-service" class="gallery terms-of-service-container">
             <div class="terms-of-service-body">
                 <p>By ordering a commission, you agree to the following terms of service, so please read it carefully.
                     Any
@@ -363,7 +241,7 @@ const comTypes = [
             </div>
         </div>
 
-        <div id="order" class="gallery order-container" style="display:none">
+        <div v-if="selectedTab == 'Order'" id="order" class="gallery order-container">
             <div class="order-body">
                 <p>To purchase a commission, you may fill the form below, if its open for responses.</p>
                 <p>Opening for commissions are announced on my Social Media and to those in the mailing list. To join my
